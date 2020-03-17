ab957b371f2969b369f8fb54a41c35d265529873
CNXYQ <1013575196@qq.com>
2020-03-17 17:20:26 +0800

    Site updated: 2020-03-17 17:20:12

---
+++ dist/music.js
@@ -0,0 +1,3 @@
+// build time:Tue Mar 17 2020 17:20:06 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"PDD洪荒之力",artist:"徐梦圆",url:"http://up.mcyt.net/?down/39868.mp3",cover:"http://oeff2vktt.bkt.clouddn.com/image/84.jpg"},{name:"9420",artist:"麦小兜",url:"http://up.mcyt.net/?down/45967.mp3",cover:"http://oeff2vktt.bkt.clouddn.com/image/8.jpg"},{name:"风筝误",artist:"刘珂矣",url:"http://up.mcyt.net/?down/46644.mp3",cover:"http://oeff2vktt.bkt.clouddn.com/image/96.jpg"}]});
+//rebuild by neat 
