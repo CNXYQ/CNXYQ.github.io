911ace7ef942f31ced6db8598a5115c8a7e59e6b
CNXYQ <1013575196@qq.com>
2020-03-19 22:52:04 +0800

    Site updated: 2020-03-19 22:51:48

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 18 2020 18:07:08 GMT+0800 (GMT+08:00)
+// build time:Thu Mar 19 2020 22:51:46 GMT+0800 (GMT+08:00)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
