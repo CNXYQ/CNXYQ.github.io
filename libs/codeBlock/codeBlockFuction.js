f3c31f85492a68c1ce928538748210b024b9966d
CNXYQ <1013575196@qq.com>
2020-06-08 20:40:50 +0800

    Site updated: 2020-06-08 20:40:50

--- libs/codeBlock/codeBlockFuction.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Jun 08 2020 20:04:08 GMT+0800 (GMT+08:00)
-$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
-//rebuild by neat 
--- libs/codeBlock/codeLang.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Jun 08 2020 20:04:08 GMT+0800 (GMT+08:00)
-$(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
-//rebuild by neat 
