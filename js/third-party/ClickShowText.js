3ddf97e698ec9dd1fa8047b788772283ff269417
CNXYQ <1013575196@qq.com>
2020-06-09 16:19:03 +0800

    Site updated: 2020-06-09 16:18:59

--- js/third-party/ClickShowText.js
+++ js/third-party/ClickShowText.js
@@ -1,3 +1,3 @@
-// build time:Tue Jun 09 2020 16:11:32 GMT+0800 (GMT+08:00)
+// build time:Tue Jun 09 2020 16:18:57 GMT+0800 (GMT+08:00)
 function co(){var t="0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f";var o=t.split(",");var a="#";for(var e=0;e<6;e++){a+=o[Math.floor(Math.random()*16)]}return a}var a_idx=0;jQuery(document).ready(function(t){t("body").click(function(o){var a=GLOBAL_CONFIG.ClickShowText;var e=a.text.split(",");var i=t("<span/>").text(e[a_idx]);a_idx=(a_idx+1)%e.length;var n=o.pageX,r=o.pageY;i.css({"z-index":150,top:r-20,left:n-40,position:"absolute","font-weight":"bold",color:co(),cursor:"default","font-size":a.fontSize||"inherit"});t("body").append(i);i.animate({top:r-180,opacity:0},1500,function(){i.remove()})})});
 //rebuild by neat 
